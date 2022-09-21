94f3b49807739f18b27ba43e5366379f5a86a568
Brian Muthui <bmuthengi@microsoft.com>
2022-09-21 13:06:16 +0300

    Log info if verbose

--- src/util/git.ts
+++ src/util/git.ts
@@ -1,9 +1,10 @@
 import * as childprocess from "child_process";
-import { verbose, positive, error } from "./format";
+import { IArgs } from "../types/args";
+import { info, positive, error } from "./format";
 
-export default function git(...args: string[]): Promise<string> {
+export default function git(argvs: IArgs, ...args: string[]): Promise<string> {
   return new Promise((resolve, reject) => {
-    verbose(`Calling: git ${args.join(" ")}`);
+    argvs.verbose && info(`Calling: git ${args.join(" ")}`);
     const gitProcess = childprocess.spawn("git", args, { cwd: process.cwd() });
     let processData = "";
     gitProcess.stdout.on("data", (data) => {
@@ -19,9 +20,10 @@ export default function git(...args: string[]): Promise<string> {
         error(`git ${args.join(" ")}:\n${processData ? processData : "error"}`);
         reject(processData);
       } else {
-        positive(
-          `git ${args.join(" ")}:\n${processData ? processData : "done"}`
-        );
+        argvs.verbose &&
+          positive(
+            `git ${args.join(" ")}:\n${processData ? processData : "done"}`
+          );
         resolve(processData);
       }
     });
